2d59620bc02f15a96aa5fbef686e57deea89197f
Leopotam <leopotam@gmail.com>
2017-10-03 11:04:37 +0300

    * ChoiceCommand - multiple choice support.

--- src/commands/choice.ts
+++ src/commands/choice.ts
@@ -2,7 +2,7 @@ import { IBot, IBotCommand, IBotCommandHelp, IBotMessage } from '../api'
 import { getRandomInt } from '../utils'
 
 export default class ChoiceCommand implements IBotCommand {
-    private readonly CMD_REGEXP = /(.+)(?: или )(.+)/i
+    private readonly CMD_REGEXP = / или /gi
 
     public getHelp(): IBotCommandHelp {
         return { caption: '{1} или {2}', description: 'Выбирает случайный вариант 1 или 2.' }
@@ -14,11 +14,7 @@ export default class ChoiceCommand implements IBotCommand {
         return this.CMD_REGEXP.test(msg)
     }
     public async process(msg: string, answer: IBotMessage): Promise<void> {
-        if (msg.toLowerCase().split(' или ').length > 2) {
-            answer.setTextOnly('сложный выбор. Предложи два варианта и я определюсь.')
-            return
-        }
-        const matches = msg.match(this.CMD_REGEXP)!
-        answer.setTextOnly(getRandomInt(1, 100) > 50 ? matches[1] : matches[2])
+        const matches = msg.replace('?', '').split(this.CMD_REGEXP)
+        answer.setTextOnly(matches[getRandomInt(0, matches.length - 1)])
     }
 }
